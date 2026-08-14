c5b71bc45c98b16f21189f6bb4de0f9133d4f34e
V. Don Rudra Pratap Srinath <vemulasrinath1464@gmail.com>
2026-08-14 14:59:22 +0530

    Time: 3 ms (73.47%) | Memory: 69.9 MB (10.67%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,31 +1,31 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int l=0;
-        int temp=0;
-        int ans=Integer.MIN_VALUE;
+        // int l=0;
+        // int temp=0;
+        // int ans=Integer.MIN_VALUE;
         
-        for(int r=0;r<nums.length;r++){
-            temp+=nums[r];
-            if(r-l+1==k){   
-                ans=Math.max(ans,temp);
-                temp-=nums[l];
-                l++;
+        // for(int r=0;r<nums.length;r++){
+        //     temp+=nums[r];
+        //     if(r-l+1==k){   
+        //         ans=Math.max(ans,temp);
+        //         temp-=nums[l];
+        //         l++;
                    
-            }   
-        } 
+        //     }   
+        // } 
         
-        return (double) ans/k;
+        // return (double) ans/k;
 
-        // int sum=0;
-        // for(int i=0;i<k;i++){
-        //     sum+=nums[i];
-        // }
-        // int maxsum=sum;
-        // for(int i=k;i<nums.length;i++){
-        //     sum+=nums[i];
-        //     sum-=nums[i-k];
-        //     maxsum=Math.max(maxsum,sum);
-        // }
-        // return (double) maxsum/k;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int maxsum=sum;
+        for(int i=k;i<nums.length;i++){
+            sum+=nums[i];
+            sum-=nums[i-k];
+            maxsum=Math.max(maxsum,sum);
+        }
+        return (double) maxsum/k;
     }
 }
