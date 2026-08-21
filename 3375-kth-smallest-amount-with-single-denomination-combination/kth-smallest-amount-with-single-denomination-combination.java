a04609208ab9227b1ecd5d775a487608f373f73a
V. Don Rudra Pratap Srinath <vemulasrinath1464@gmail.com>
2026-08-21 21:30:37 +0530

    Time: 198 ms (7.84%) | Memory: 43.6 MB (78.43%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,79 @@
+class Solution {
+
+    long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    public long findKthSmallest(int[] coins, int k) {
+
+        long low = 1;
+
+        long high = (long) coins[0] * k;
+
+        for (int coin : coins) {
+            high = Math.min(high, (long) coin * k);
+        }
+
+        while (low < high) {
+
+            long mid = low + (high - low) / 2;
+
+            if (count(mid, coins) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    long count(long x, int[] coins) {
+
+        int n = coins.length;
+        long total = 0;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+
+            long lcm = 1;
+            int bits = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                if ((mask & (1 << i)) != 0) {
+
+                    bits++;
+
+                    long g = gcd(lcm, coins[i]);
+
+                    // Prevent LCM from becoming larger than x
+                    if (lcm > x / (coins[i] / g)) {
+                        lcm = x + 1;
+                        break;
+                    }
+
+                    lcm = (lcm / g) * coins[i];
+                }
+            }
+
+            if (lcm > x) {
+                continue;
+            }
+
+            long multiples = x / lcm;
+
+            if (bits % 2 == 1) {
+                total += multiples;
+            } else {
+                total -= multiples;
+            }
+        }
+
+        return total;
+    }
+}
