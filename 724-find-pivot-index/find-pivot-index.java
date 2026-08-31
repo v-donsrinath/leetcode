5e994a813626a85ea85e65acb467ee33cf6e2f8a
V. Don Rudra Pratap Srinath <vemulasrinath1464@gmail.com>
2026-08-31 12:59:04 +0530

    Time: 314 ms (7.56%) | Memory: 47.6 MB (22.68%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        for(int i=0;i<nums.length;i++){
+            int lsum=0;
+            for(int j=0;j<i;j++){
+                lsum+=nums[j];
+            }
+            int rsum=0;
+            for(int k=i+1;k<nums.length;k++){
+                rsum+=nums[k];;    
+            }
+            if(lsum==rsum){
+                return i;
+            }
+        }
+        return -1;
+    }   
+}
