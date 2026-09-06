e3efc4752c007ee327111d9dc813ceeb41efe2ca
V. Don Rudra Pratap Srinath <vemulasrinath1464@gmail.com>
2026-09-06 16:43:19 +0530

    Time: 1 ms (98.87%) | Memory: 42.8 MB (8.74%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        long l=1;
+        long r=x/2;
+
+        while(l<=r){
+            long mid=l+(r-l)/2;
+            long square=mid*mid;
+             
+            if(square==x){
+                return (int) mid;
+            }
+            if(square<x){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+
+        }
+        return (int) r;
+    }
+}
