f935154d3734720ed407a1377a0d0990f7251dde
V. Don Rudra Pratap Srinath <vemulasrinath1464@gmail.com>
2026-09-13 00:09:54 +0530

    Time: 5 ms (64.88%) | Memory: 47 MB (55.35%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,40 +1,32 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        int n=asteroids.length;
         Stack<Integer> st=new Stack<>();
-        
-        for(int i=0;i<asteroids.length;i++){
+        for(int i=0;i<n;i++){
             boolean alive=true;
             int current=asteroids[i];
             if(current>0){
                 st.push(current);
             }
             else{
-                while (!st.isEmpty() && st.peek() > 0 
-                        && st.peek() < Math.abs(current)) {
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(current)){
                     st.pop();
                 }
                 if(st.isEmpty() || st.peek()<0){
                     st.push(current);
                 }
-                else if (st.peek() == Math.abs(current)) {
+                else if(st.peek()==Math.abs(current)){
                     st.pop();
                 }
-
-                // Top is bigger -> current asteroid explodes
-                else {
-                    alive = false;
+                else{
+                    alive=false;
                 }
             }
-         }
-         // Convert stack to array
-        int[] ans = new int[st.size()];
-
-        for (int i = 0; i < st.size(); i++) {
-            ans[i] = st.get(i);
         }
-
+        int[] ans=new int[st.size()];
+        for(int i=0;i<st.size();i++){
+            ans[i]=st.get(i);
+        }
         return ans;
-
-        
     }
 }
